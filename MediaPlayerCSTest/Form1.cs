352b112eb2240d0d2e54b1150b66670d17458954
Sarah <sarah.busert@hs-furtwangen.de>
2015-06-23 23:37:58 +0200

    First

---
+++ MediaPlayerCSTest/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaPlayerCSTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            axWindowsMediaPlayer1.URL = @"testVideo.mp4";
+            axWindowsMediaPlayer1.uiMode = "none";
+        }
+
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                Application.Exit();
+            }
+
+        }
+    }
+}
