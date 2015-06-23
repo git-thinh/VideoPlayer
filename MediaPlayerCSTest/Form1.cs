9e64fbb2fd4600467e99f016734340c89cad50c5
Sarah Busert <sarah.busert@hs-furtwangen.de>
2015-06-24 00:14:19 +0200

    Added var for path to user directory

--- MediaPlayerCSTest/Form1.cs
+++ MediaPlayerCSTest/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,7 +23,14 @@ namespace MediaPlayerCSTest
             this.TopMost = true;
             this.FormBorderStyle = FormBorderStyle.None;
             this.WindowState = FormWindowState.Maximized;
-            axWindowsMediaPlayer1.URL = @"testVideo.mp4";
+
+            string path = Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)).FullName;
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                path = Directory.GetParent(path).ToString();
+            }
+
+            axWindowsMediaPlayer1.URL = path + @"\Documents\GitHub\MediaPlayer\testVideo.mp4";
             axWindowsMediaPlayer1.uiMode = "none";
         }
 
